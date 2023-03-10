f928a0419b12f943370138a22c86e032674ea5f7
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-03-10 11:14:18 +0100

    frissites

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 window.addEventListener("load", init);
 
-function init(){
-    const articleElem = document.querySelectorAll("article");
-    var lista = ["<div><img src='kep.jpg' alt='kep' ></div>", "<div><img src='kep2.jpg' alt='kep2' ></div>"]
-    for (let index = 0; index < lista.length; index++) {
-        articleElem[0].innerHTML = lista[index];
-        
-    }
+function init() {
+  let lista = [
+    "kep.jpg",
+    "kep2.jpg",
+    "kep3.jpg",
+    "kep4.jpg",
+    "kep5.jpg",
+    "kep6.jpg",
+    "kep7.jpg",
+    "kep8.jpg",
+  ];
+  const ARTICLEELEM = document.querySelectorAll("article");
+  for (let index = 0; index < lista.length; index++) {
+    ARTICLEELEM[0].innerHTML +=
+      "<div><img src='" + lista[index] + "' alt='kep'></div>";
+  }
+  const KEPELEM = document.querySelectorAll("article img");
+  console.log(KEPELEM);
+  let mainElem = document.querySelectorAll("section img");
+  let foKep = document.querySelectorAll("section div img");
+  let aktElem = 0;
+  for (let index = 0; index < lista.length; index++) {
+    KEPELEM[index].addEventListener("click", function () {
+      foKep[0].src = lista[index];
+      aktElem = index;
+    });
+  }
 
+  const BALELEM = document.querySelectorAll(".bal");
+  const JOBBELEM = document.querySelectorAll(".jobb");
+
+  JOBBELEM[0].addEventListener("click", function () {
+    aktElem++;
+    foKep[0].src = lista[aktElem];
+    console.log(aktElem);
+  });
+  BALELEM[0].addEventListener("click", function () {
+    aktElem--;
+    foKep[0].src = lista[aktElem];
+    console.log(aktElem);
+  });
 }
